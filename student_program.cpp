52cbbb001b2a6a1c82a317b8d3d2fd3afda7acb0
Phongsaphat <164649051+whatthebook@users.noreply.github.com>
2025-03-08 12:24:27 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,9 +1,11 @@
-void GetMatrix(int **value, int *row, int *col) {
-    printf("Enter values for the %d x %d matrix:\n", *row, *col);
-    for (int i = 0; i < *row; i++) {
-        for (int j = 0; j < *col; j++) {
-            printf("Enter element (%d, %d): ", i + 1, j + 1);
-            scanf("%d", &value[i][j]); // รับค่าจากผู้ใช้
-        }
+#include <stdio.h>
+
+int main() {
+    int number ; 
+    scanf( "%d", &number ) ; 
+
+    for( int i = 1 ; i <= number ; i++ ) { 
+        printf( "[%d] Hello World\n", i ) ; 
     }
-}
+    return 0 ;
+}//end functio
